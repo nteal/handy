e47933e22823ea6143c8b4d1447d0501855ae497
Nick Teal <nteal15@gmail.com.com>
2019-06-19 23:24:03 -0500

    adds contains key and simple test

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -2,6 +2,40 @@ class helpers {
     public static isFalsy = (value: any, strict?: any) => {
         return strict ? (value === null || value === undefined) : !!value;
     }
+
+    public static containsKeys = (obj: any, ...properties: string[]) => {
+        return properties.every((property: string) => helpers.containsKey(obj, property));
+    }
+
+    // recursive
+    // public static containsKey = (obj: any, prop: string): boolean => {
+    //     if (!prop || !obj) return false;
+    //     const propArr = prop.split('.');
+    //     if (propArr.length === 1) {
+    //         return obj.hasOwnProperty(prop);
+    //     }
+    //     if (propArr.length === 0) {
+    //         return false;
+    //     }
+    //     else {
+    //         const firstProp: string | undefined = propArr.shift();
+    //         return firstProp && obj.hasOwnProperty(firstProp) && helpers.containsKey(obj[firstProp], propArr.join('.'));
+    //     }
+    // }
+
+    // iterative
+    public static containsKey = (obj: any, prop: string): boolean => {
+        const propertyArr = prop.split('.');
+        let testObj = obj;
+        for (let i = 0; i < propertyArr.length; i++) {
+            let prop = propertyArr[i];
+            if (!testObj.hasOwnProperty(prop)) {
+                return false;
+            }
+            testObj = testObj[prop];
+        }
+        return true;
+    }
 }
 
 export default helpers;
--- src/testApp.ts
+++ src/testApp.ts
@@ -1,3 +1,14 @@
 import handy from './lib/index';
 
 console.log(handy.isFalsy(null, 1));
+
+const testObj = { 'test': 'foundTest!', nextLevel: { test: 'foundTest!', deeper: { deeper: { deepest: true } } } }
+const timerLabel = 'containsKeys'
+console.time(timerLabel);
+console.log(!handy.containsKeys(testObj, 'false')) // should be false
+console.log(handy.containsKeys(testObj, 'test')) // should be true
+console.log(handy.containsKeys(testObj, 'test', 'nextLevel.test')) // should be true
+console.log(handy.containsKeys(testObj, 'test', 'nextLevel.test', 'nextLevel.deeper.deeper.deepest')) // should be true
+console.log(!handy.containsKeys(testObj, 'test', 'nextLevel.test', 'false')) // should be false
+console.log(!handy.containsKeys(testObj, 'test.test.test.test')) // should be false
+console.timeEnd(timerLabel);
