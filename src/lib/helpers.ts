aa3bfa4e8e56e5a38fa0cd34fba6a377adc775ee
Nick Teal <nteal15@gmail.com.com>
2019-06-19 23:38:56 -0500

    updates containsFunction

--- src/lib/helpers.ts
+++ src/lib/helpers.ts
@@ -7,34 +7,19 @@ class helpers {
         return properties.every((property: string) => helpers.containsKey(obj, property));
     }
 
-    // recursive
-    // public static containsKey = (obj: any, prop: string): boolean => {
-    //     if (!prop || !obj) return false;
-    //     const propArr = prop.split('.');
-    //     if (propArr.length === 1) {
-    //         return obj.hasOwnProperty(prop);
-    //     }
-    //     if (propArr.length === 0) {
-    //         return false;
-    //     }
-    //     else {
-    //         const firstProp: string | undefined = propArr.shift();
-    //         return firstProp && obj.hasOwnProperty(firstProp) && helpers.containsKey(obj[firstProp], propArr.join('.'));
-    //     }
-    // }
-
-    // iterative
     public static containsKey = (obj: any, prop: string): boolean => {
-        const propertyArr = prop.split('.');
-        let testObj = obj;
-        for (let i = 0; i < propertyArr.length; i++) {
-            let prop = propertyArr[i];
-            if (!testObj.hasOwnProperty(prop)) {
-                return false;
-            }
-            testObj = testObj[prop];
+        if (!prop || !obj) return false;
+        const propArr = prop.split('.');
+        if (propArr.length === 1) {
+            return obj.hasOwnProperty(prop);
         }
-        return true;
+        if (propArr.length === 0) {
+            return false;
+        }
+        else {
+            const firstProp: string | undefined = propArr.shift();
+            return firstProp && obj.hasOwnProperty(firstProp) && helpers.containsKey(obj[firstProp], propArr.join('.'));
+        }
     }
 }
 
