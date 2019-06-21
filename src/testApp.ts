079b4802610b154f18a8c1c22859d543b6168298
Nick Teal <nteal15@gmail.com.com>
2019-06-20 20:47:20 -0500

    adds safeVal, slight update to containsKey

--- src/testApp.ts
+++ src/testApp.ts
@@ -3,12 +3,15 @@ import handy from './lib/index';
 console.log(handy.isFalsy(null, 1));
 
 const testObj = { 'test': 'foundTest!', nextLevel: { test: 'foundTest!', deeper: { deeper: { deepest: true } } } }
-const timerLabel = 'containsKeys'
-console.time(timerLabel);
+
 console.log(!handy.containsKeys(testObj, 'false')) // should be false
 console.log(handy.containsKeys(testObj, 'test')) // should be true
 console.log(handy.containsKeys(testObj, 'test', 'nextLevel.test')) // should be true
 console.log(handy.containsKeys(testObj, 'test', 'nextLevel.test', 'nextLevel.deeper.deeper.deepest')) // should be true
 console.log(!handy.containsKeys(testObj, 'test', 'nextLevel.test', 'false')) // should be false
 console.log(!handy.containsKeys(testObj, 'test.test.test.test')) // should be false
-console.timeEnd(timerLabel);
+
+console.log(handy.safeVal(testObj, 'notFound') === null) //defaults to null
+console.log(handy.safeVal(testObj, 'notFound', 'not found') === 'not found') //accepts defaultVal
+console.log(handy.safeVal(testObj, 'test', 'not found') === testObj.test) //returns value if found
+console.log(handy.safeVal(testObj, 'nextLevel.deeper.deeper.deepest', 'not found') === testObj.nextLevel.deeper.deeper.deepest) //returns nested value if found
